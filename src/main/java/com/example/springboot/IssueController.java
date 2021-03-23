e2eab5234b5908a1c93d5912e66fd383de6b2a92
pccmast <248981941@qq.com>
2021-03-23 16:03:11 +0800

    给应用增加功能，使其能接受并处理post请求

--- src/main/java/com/example/springboot/IssueController.java
+++ src/main/java/com/example/springboot/IssueController.java
@@ -5,12 +5,17 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * 参照github rest api对url进行处理
+ */
 @RestController
 @RequestMapping("repos")
 public class IssueController {
     @DeleteMapping("/{owner}/{repo}/issues/{issueNumber}/lock")
+    // 删除一个issue
     public void unlock(
             @PathVariable("owner") String owner,
             @PathVariable("repo") String repo,
@@ -22,6 +27,7 @@ public class IssueController {
     }
 
     @PostMapping("/{owner}/{repo}/issues")
+    // 创建一个issue
     public void create(
             @PathVariable("owner") String owner,
             @PathVariable("repo") String repo,
@@ -30,5 +36,11 @@ public class IssueController {
         System.out.println(owner);
         System.out.println(repo);
         System.out.println(requestBody);
+    }
+
+    @PostMapping("/demo")
+    public void formDemo(@RequestParam("aaa") String aaa, @RequestParam("bbb") String bbb) {
+        System.out.println(aaa);
+        System.out.println(bbb);
     }
 }
