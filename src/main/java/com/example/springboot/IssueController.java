4cedb873a4f07216003680b755d894e4411bef3f
pccmast <248981941@qq.com>
2021-03-23 15:02:47 +0800

    给应用增加功能，使其能接受并处理post请求

--- src/main/java/com/example/springboot/IssueController.java
+++ src/main/java/com/example/springboot/IssueController.java
@@ -2,6 +2,8 @@ package com.example.springboot;
 
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,5 +19,14 @@ public class IssueController {
         System.out.println("owner: " + owner);
         System.out.println("repo: " + repo);
         System.out.println("issueNumber:　" + issueNumber);
+    }
+
+    @PostMapping("/{owner}/{repo}/issues")
+    public void create(
+            @PathVariable("owner") String owner,
+            @PathVariable("repo") String repo,
+            @RequestBody Object requestBody
+    ) {
+        System.out.println(requestBody);
     }
 }
